63ecc569290eff2b1f53557405b78ec98f3e7e67
JamesTsaiJT <43771177+JamesTsaiJT@users.noreply.github.com>
2023-07-01 15:32:51 +0800

    Add files via upload

---
+++ L10-socket_server.py
@@ -0,0 +1,19 @@
+import socket
+
+serverPort = 5000
+
+serverSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serverSocket.bind(("192.168.3.8",serverPort))
+
+print("ready")
+
+while(True):
+    message, clientAddress = serverSocket.recvfrom(2048)    
+    if(message.decode() == "request"):
+        message = "Liam is good boy"
+        serverSocket.sendto(message.encode(),clientAddress)
+    else:
+        message = "Liam is bad boy"
+        serverSocket.sendto(message.encode(),clientAddress)
+
+serverSocket.close()
